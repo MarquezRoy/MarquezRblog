1b47df925897cf7d65a70c7151083a5bc9cacd1d
Roy Marquez <theroyal3655@gmail.com>
2015-01-28 14:03:34 -0800

    2nd commit
    thingy

--- controller/create-db.php
+++ controller/create-db.php
@@ -1,5 +1,17 @@
 <?php
-require_once("../model/database.php");
+require_once(__DIR__ . "/../model/database.php");
 
-$connection = new mysquli($host, $username, $password);
+$connection = new mysqli($host, $username, $password);
+
+if($connection->connect_error) {
+    die("Error: " . $connection->connect_error);
+}
+
+$exists = $connection->select_db($database);
+
+if($exists) {
+    echo "Database exists";
+}
+
+$connection->close();
 
