3d2b76a6678a2e301c054dd70dd3f48250543990
Roy Marquez <theroyal3655@gmail.com>
2015-02-10 13:11:53 -0800

    commitin

--- controller/create-post.php
+++ controller/create-post.php
@@ -1,3 +1,7 @@
 <?php
+$title = filer_input(INPUT_POST, "title", FILTER_SANITIZE_STRING);
+$post = filter_input(INPUT_POST, "post", FILTER_SANITIZE_STRING);
 
+echo "<p>Title: $title</p>";
+echo "<p>Post: $post</p>";
 
--- view/form.php
+++ view/form.php
@@ -1,6 +1,6 @@
 <h1>Create Blog Post</h1>
 
-<form>
+<form method="post">
     <div>
         <label for="title">Title: </label>
         <input type="text" name="title"/>
@@ -10,8 +10,8 @@
         <label for="post">Post: </label>
         <textarea name="post"></textarea>
     </div>
-    
+
     <div>
         <button type="submit">Submit</button>  
     </div>
-    </form>
+</form>
