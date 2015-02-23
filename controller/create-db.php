9ff23e51e239ebe52d04f6f4764238f82934ff4a
Roy Marquez <theroyal3655@gmail.com>
2015-02-23 14:16:50 -0800

    Sweg

--- controller/create-db.php
+++ controller/create-db.php
@@ -2,7 +2,7 @@
 
 require_once(__DIR__ . "/../model/config.php");
 
-$query = $connection->query("CREATE TABLE posts ("
+$query = $_SESSION["connection"]->query("CREATE TABLE posts ("
         . "id int(11) NOT NULL AUTO_INCREMENT,"
         . "title varchar(255) NOT NULL,"
         . "post text NOT NULL,"
@@ -13,8 +13,26 @@ if ($query) {
 } 
 else {
 
-    echo "<center><p>$connection->error</p></center>";
+    echo "<center><p>" . $_SESSION["connection"]->error . "</p></center>";
 }
+
+$query = $_SESSION["connection"]->query("CREATE TABLE users("
+        . "id int(11) NOT NULL AUTO_INCREMENT,"
+        . "username varchar(30) NOT NULL,"
+        . "email varchar(50) NOT NULL,"
+        ."password char(128) NOT NULL, "
+        . "salt char(128) NOT NULL,"
+        . "PRIMARY KEY (id))");
+
+if($query) {
+    echo "<center><p>Succesfullly created table users</p></center>";
+}
+else {
+    echo "<p>" . $_SESSION["connection"]->error . "</p>";
+}
+        
+
+
 
 
 
